efd6a2267d0a67576f3607b6fd30e6eea1d8a1ab
YuriSantxz07 <yuri.santos.tds24@gmail.com>
2025-09-04 14:08:34 -0300

    Upd

--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SenaiCommunity.BackEnd.Repository;
-
-import com.SenaiCommunity.BackEnd.Entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
